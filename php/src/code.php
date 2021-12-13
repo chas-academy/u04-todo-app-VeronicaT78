8ea4ff3588799742954236d18167eb67bebe8e91
VeronicaT78 <70715635+VeronicaT78@users.noreply.github.com>
2021-12-13 14:02:09 +0100

    submit task

---
+++ php/src/code.php
@@ -0,0 +1,28 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    require('dbconnect.php');
+
+    $tasktitle = $_POST['tasktitle'];
+    $taskdesc = $_POST['taskdesc'];
+    $type = $_POST['type'];
+
+
+        $sql = "INSERT INTO todolist (tasktitle, taskdesc, type) VALUES ( :tasktitle, :taskdesc, :type)";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute(['tasktitle' =>$tasktitle, 'taskdesc'=>$taskdesc, 'type'=>$type]);
+
+        if($stmt){
+            header('location: index.php?mess=success'); 
+        }else {
+            header('location: index.php');
+        }
+        $pdo = null;
+        exit();
+    
+}
+
+    
+    
+
+
