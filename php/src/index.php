4bf851821ebfd404ca6990ef8ee4657311a674d7
VeronicaT78 <70715635+VeronicaT78@users.noreply.github.com>
2021-12-13 18:03:59 +0100

    changes to front page

--- php/src/index.php
+++ php/src/index.php
@@ -6,42 +6,25 @@
     
 ?>
 <div class="form">
-
-<p style="color:red"><?php if(!empty($msg)){echo $msg; }?></p>
-    
-    <form action="code.php" method="POST" class="input_form">
-        <div class="form_group">
-            <?php if (isset($errors)) { ?>
-                <p><?php echo $errors; ?></p>
-            <?php } ?>
-            <label for="text">Task:</label>
-            <input type="text" name="tasktitle" class="form_task">
-        </div>
-
-        <div class="form_group">
-            <label for="">Choose a type:</label>
-                <select name="type" id="type" class="form_control">
-                    <option value="">-Select type-</option>
-                    <option value="home">Home</option>
-                    <option value="work">Work</option>
-                    <option value="leisure">Leisure</option>
-                    <option value="family">Family</option>
-                </select>
-        </div>
-
-        <div class="form_group">
-            <label for="textarea">Description:</label>
-                <textarea name="taskdesc" class="textarea" rows="4" cols="50">
-                </textarea>
-        </div>
-
-        <div class="form_group">
-            <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
-        </div>
-        
-    </form>
-
+    <p style="color:red"><?php if(!empty($msg)){echo $msg; }?></p>
+        <?php include './crud/addtask.php';?>
 </div>
-    
+
+<div class="table">
+    <table>
+        <tr>
+            <th>Task</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Done</th>
+        </tr>
+        
+            <?php include './crud/showtask.php';?>
+            <td>
+				<a href="index.php?edit=<?php echo $tasks['id']; ?>" class="edit_btn" >Edit</a>
+			</td>
+        
+    </table>
+</div>
 
 
