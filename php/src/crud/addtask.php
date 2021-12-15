b946065221daf36af85c34af19b66a17a1d8dec5
VeronicaT78 <70715635+VeronicaT78@users.noreply.github.com>
2021-12-15 15:36:27 +0100

    make addtask/update button

--- php/src/crud/addtask.php
+++ php/src/crud/addtask.php
@@ -25,7 +25,8 @@
                 </textarea>
         </div>
         <div class="form_group">
-            <!-- <button class="btn" type="submit" name="update" style="background: #556B2F;" >Update</button> -->
-            <button class="btn" type="submit" name="submit" >Add task</button>
+            <?php if(isset($_GET['id'])){ echo '<button class="btn" type="submit" name="update" style="background: #556B2F;" >Update</button>';}
+                else{ echo '<button class="btn" type="submit" name="submit" >Add task</button>';} 
+            ?>
         </div>
 </form>
